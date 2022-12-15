a5fb058cc7ec37a30aa529d94cbbcc275f2fcc54
Ashley Stewart <a.stewart.au@gmail.com>
2022-12-15 16:20:57 +1000

    Changes

--- prepare_segmentations.py
+++ prepare_segmentations.py
@@ -10,3 +10,11 @@ for seg_file in seg_files:
     seg = np.array(seg == 2, dtype=seg.dtype)
     nib.save(nib.Nifti1Image(seg, header=nii.header, affine=nii.affine), f"{seg_file.split('.')[0]}_seeds.nii")
 
+for seg_file in seg_files:
+    nii = nib.load(seg_file)
+    seg = nii.get_fdata()
+    seg[seg == 1] = 0 # prostate
+    seg[seg == 2] = 1 # seed
+    seg[seg == 3] = 2 # calc
+    nib.save(nib.Nifti1Image(seg, header=nii.header, affine=nii.affine), f"{seg_file.split('.')[0]}_seeds-calc.nii")
+
